6972cba15e6a585fdc6462a97c1851fc6d1e43c7
WebDevStar <webpartner0319@outlook.com>
2021-01-11 22:03:29 +0300

    Infrastructure setup for backend and auth.

---
+++ backend/settings.py
@@ -0,0 +1,128 @@
+import os
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = '-ml4+vhcou8l=fg$#a=r+4(wpw44y9^y(rhv!h9y6o$4msw$)*'
+USE_AWS = os.getenv("USE_AWS", "false")
+DEBUG = True
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+REST_AUTH_TOKEN_MODEL = "backend.models.CustomToken"
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'backend.urls'
+
+if USE_AWS:
+    INSTALLED_APPS.append('storages')
+    AWS_ACCESS_KEY_ID = os.getenv("AWS_S3_ACCESS_ID")
+    AWS_SECRET_ACCESS_KEY = os.getenv("AWS_S3_ACCESS_KEY")
+
+    if not AWS_ACCESS_KEY_ID or not AWS_SECRET_ACCESS_KEY:
+        raise ValueError("AWS_ACCESS variables are not set")
+
+    AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_S3_BUCKET", 'dev-media-360')
+    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+    AWS_DEFAULT_ACL = "public-read"
+    MEDIA_LOCATION = 'media'  # Subfolder path in bucket where images and uploads are stored
+    MEDIA_URL = f'http://{AWS_S3_CUSTOM_DOMAIN}/{MEDIA_LOCATION}/'
+    DEFAULT_FILE_STORAGE = 'backend.storage.PublicMediaStorage'
+    STATIC_URL = '/s/'
+    STATIC_ROOT = 'static/'
+else:
+    STATIC_URL = '/s/'
+    STATIC_ROOT = 'static/'
+    MEDIA_URL = "/files/"
+    MEDIA_ROOT = "uploads/"
+    MEDIA_LOCATION = ''
+
+FILE_UPLOAD_MAX_MEMORY_SIZE = 524288000
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        ,
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'backend.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['RDS_360_DB_NAME'],
+        'USER': os.environ['RDS_USERNAME'],
+        'PASSWORD': os.environ['RDS_PASSWORD'],
+        'HOST': os.environ['RDS_HOSTNAME'],
+        'PORT': os.environ['RDS_PORT'],
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication', # we're going to remove this
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ]
+}
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+FILE_UPLOAD_MAX_MEMORY_SIZE = 524288000
+
+CORS_ALLOW_ALL_ORIGINS = True
